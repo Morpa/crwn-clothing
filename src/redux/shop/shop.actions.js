803b2b896a2f9aca89d64a5617490bf02ae25a6a
André Morpanini <andremorpanini@MacBook-Pro-de-Andre.local>
2019-12-26 17:37:24 +0000

    Add Redux Thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,35 @@
 import ShopActionsTypes from './shop.types';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionsTypes.UPDATE_COLLECTIONS,
+import {
+  firestore,
+  convertCollectionSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
